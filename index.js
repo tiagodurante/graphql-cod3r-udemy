8e60e4084e9b75274233d6043a79b7a11dfc4224
Tiago Amado Durante <tiagodurante@icloud.com>
2019-09-03 11:36:48 -0300

    tipos personalizados

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 const { ApolloServer, gql } = require("apollo-server");
 
 const typeDefs = gql`
+  scalar Date
   # Pontos de entrada da sua API
+  type Usuario {
+    id: ID!
+    nome: String!
+    email: String!
+    idade: Int
+    salario: Float
+    vip: Boolean
+  }
+
   type Query {
-    horaAtual: String!
+    horaAtual: Date
+    usuarioLogado: Usuario
   }
 `;
 
 const resolvers = {
   Query: {
     horaAtual() {
-      const today = new Date();
-      const dd = String(today.getDate()).padStart(2, "0");
-      const mm = String(today.getMonth() + 1).padStart(2, "0");
-      const yyyy = today.getFullYear();
-      return String(`${dd}/${mm}/${yyyy}`);
+      return new Date();
+    },
+    usuarioLogado() {
+      return {
+        id: 1,
+        nome: "Tiago",
+        email: "tiagodurante@outlook.com.br",
+        idade: "23",
+        salario: 1234.56,
+        vip: true
+      };
     }
   }
 };
